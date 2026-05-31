8852691bae8fc393e34b3353bf702c3ce2dfb701
vanneshaa <tiaravanes73@gmail.com>
2026-05-31 09:46:49 +0700

    fix missing react hooks import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import SplashScreen from "./components/SplashScreen";
